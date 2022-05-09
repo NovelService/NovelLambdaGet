8c7dd78b7b8f556736049d56176d8856396add30
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-09 18:31:51 +0200

    Switch to AWS SDK v2

--- src/main/kotlin/com/xiangronglin/novel/lambda/get/Handler.kt
+++ src/main/kotlin/com/xiangronglin/novel/lambda/get/Handler.kt
@@ -1,23 +1,24 @@
 package com.xiangronglin.novel.lambda.get
 
-import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder
-import com.amazonaws.services.dynamodbv2.document.DynamoDB
-import com.amazonaws.services.dynamodbv2.document.PrimaryKey
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbTable
+import software.amazon.awssdk.enhanced.dynamodb.Key
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema
 
 class Handler(
     private val logger: Logger,
     private val config: Config,
-    private val dynamoDB: DynamoDB = DynamoDB(AmazonDynamoDBClientBuilder.defaultClient()),
+    private val dynamoDB: DynamoDbEnhancedClient = DynamoDbEnhancedClient.create(),
 ) {
     fun handleRequest(request: Request): Response {
         logger.log("Handling request. id ${request.id}")
 
-        val table = dynamoDB.getTable(config.dynamoDBConfig.tableName)
-        logger.log("Getting item from db. id ${request.id}")
-        val item = table.getItem(PrimaryKey("id", request.id.toString()))
-        logger.log("Successfully got item from db. id ${request.id}")
-
+        val table: DynamoDbTable<Response> = dynamoDB.table(
+            config.dynamoDBConfig.tableName, TableSchema.fromBean(
+                Response::class.java
+            )
+        )
         logger.log("Finished handling request. id ${request.id}")
-        return Response(request.id, item.getString("status"), item.getString("url"))
+        return table.getItem(Key.builder().partitionValue(request.id.toString()).build())
     }
 }
--- src/main/kotlin/com/xiangronglin/novel/lambda/get/Response.kt
+++ src/main/kotlin/com/xiangronglin/novel/lambda/get/Response.kt
@@ -1,9 +1,13 @@
 package com.xiangronglin.novel.lambda.get
 
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey
 import java.util.UUID
 
+@DynamoDbBean
 data class Response(
-    val id: UUID,
-    val status: String?,
-    val url: String?
+    @get:DynamoDbPartitionKey
+    var id: UUID = UUID.randomUUID(),
+    var status: String? = null,
+    var url: String? = null
 )
