cba814213c07af8e9e7cb1d06c1f47b80480f82e
XiangRongLin <41164160+XiangRongLin@users.noreply.github.com>
2022-05-09 18:45:08 +0200

    Remove boilerplate and use static initialization

--- src/main/kotlin/com/xiangronglin/novel/lambda/get/AwsHandler.kt
+++ src/main/kotlin/com/xiangronglin/novel/lambda/get/AwsHandler.kt
@@ -4,28 +4,37 @@ import com.amazonaws.services.lambda.runtime.Context
 import com.amazonaws.services.lambda.runtime.RequestStreamHandler
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonParser
+import software.amazon.awssdk.enhanced.dynamodb.DynamoDbEnhancedClient
+import software.amazon.awssdk.enhanced.dynamodb.Key
+import software.amazon.awssdk.enhanced.dynamodb.TableSchema
 import java.io.InputStream
 import java.io.OutputStream
 
 class AwsHandler : RequestStreamHandler {
-    private val gson = GsonBuilder().create()
-    private val config = ConfigManager().config
+    companion object {
+        private val gson = GsonBuilder().create()
+        private val config = ConfigManager.config
+        private val dynamoTable = DynamoDbEnhancedClient.create()
+            .table(config.dynamoDBConfig.tableName, TableSchema.fromBean(Response::class.java))
+    }
 
     override fun handleRequest(input: InputStream, output: OutputStream, context: Context) {
         val reader = input.reader()
         val writer = output.writer()
-        val logger = Logger(context.logger)
 
         try {
             val event = JsonParser.parseReader(reader)
-            logger.log("function called")
+            context.logger.log("function called with event $event")
 
             val request = gson.fromJson(event.asJsonObject.get("body").asString, Request::class.java)
-            val response = Handler(logger, config).handleRequest(request)
+            context.logger.log("Handling request. id ${request.id}")
+
+            val response = dynamoTable.getItem(Key.builder().partitionValue(request.id.toString()).build())
+            context.logger.log("Retrieved from db. id ${request.id}")
 
             writer.write(gson.toJson(response))
         } catch (e: java.lang.Exception) {
-            logger.log("ERROR occurred: $e")
+            context.logger.log("ERROR occurred: $e")
         } finally {
             reader.close()
             writer.close()
--- src/main/kotlin/com/xiangronglin/novel/lambda/get/ConfigManager.kt
+++ src/main/kotlin/com/xiangronglin/novel/lambda/get/ConfigManager.kt
@@ -1,18 +1,12 @@
 package com.xiangronglin.novel.lambda.get
 
-class ConfigManager {
 
-    companion object {
-        private const val DYNAMO_DB_TABLE_NAME_KEY = "DYNAMO_DB_TABLE_NAME"
-    }
+object ConfigManager {
+    private const val DYNAMO_DB_TABLE_NAME_KEY = "DYNAMO_DB_TABLE_NAME"
 
-    val config: Config
-
-    init {
-        config = Config(
-            dynamoDBConfig = DynamoDBConfig(System.getenv(DYNAMO_DB_TABLE_NAME_KEY))
-        )
-    }
+    val config: Config = Config(
+        dynamoDBConfig = DynamoDBConfig(System.getenv(DYNAMO_DB_TABLE_NAME_KEY))
+    )
 
 }
 
